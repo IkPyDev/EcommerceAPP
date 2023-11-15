0354222e83e2beb2bdc07faa7835c04e061b3c9c
IkPyDev <Ikpydev@gmail.com>
2023-11-15 23:05:53 +0500

    Home Ui Finished

--- app/src/main/java/com/ikpydev/ecommerceapp/presention/sign_up/SignUpFragment.kt
+++ app/src/main/java/com/ikpydev/ecommerceapp/presention/sign_up/SignUpFragment.kt
@@ -1,10 +1,19 @@
 package com.ikpydev.ecommerceapp.presention.sign_up
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -57,6 +66,55 @@ class SignUpFragment : Fragment() {
             findNavController().navigate(SignInFragmentDirections.toSignupFragment())
         }
 
+        val tersmOf = getString(R.string.terms_of_use)
+        val privacyPolise = getString(R.string.privacy_police)
+        val string = getString(R.string.sign_up_pr,tersmOf,privacyPolise)
+        val spannableString = SpannableString(string)
+        val termOfSpan = object : ClickableSpan() {
+            override fun onClick(p0: View) {
+                val intet = Intent(Intent.ACTION_VIEW, Uri.parse("https://mohirdev.uz/"))
+                startActivity(intet)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+                ds.color = ContextCompat.getColor(requireContext(), R.color.orange)
+                val semisBold = ResourcesCompat.getFont(requireContext(),R.font.semis_bold)
+                ds.typeface = semisBold
+            }
+        }
+
+        val privacyPoliseSpan = object : ClickableSpan() {
+            override fun onClick(p0: View) {
+                val intet = Intent(Intent.ACTION_VIEW, Uri.parse("https://praktikum.mohirdev.uz/"))
+                startActivity(intet)
+
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+                ds.color = ContextCompat.getColor(requireContext(), R.color.orange)
+                val semisBold = ResourcesCompat.getFont(requireContext(),R.font.semis_bold)
+                ds.typeface = semisBold
+            }
+        }
+        spannableString.setSpan(
+            termOfSpan,
+            string.indexOf(tersmOf),
+            string.indexOf(tersmOf) + tersmOf.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        spannableString.setSpan(
+            privacyPoliseSpan,
+            string.indexOf(privacyPolise),
+            string.indexOf(privacyPolise) + privacyPolise.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        terms.text = spannableString
+        terms.movementMethod = LinkMovementMethod.getInstance()
+
 
     }
 
@@ -66,8 +124,8 @@ class SignUpFragment : Fragment() {
             register.text =
                 if (isloading) null else root.context.getString(R.string.fragment_register)
         }
-        viewModel.events.observe(viewLifecycleOwner){
-            when(it){
+        viewModel.events.observe(viewLifecycleOwner) {
+            when (it) {
                 SignUpViewModel.Events.AlreadyRegistered -> toast(R.string.already)
                 SignUpViewModel.Events.ConnectionError -> toast(R.string.connect_error)
                 SignUpViewModel.Events.Error -> toast(R.string.Error)
