c76bc17f6488d477ce5e950a0e4533975a1e319b
IkPyDev <Ikpydev@gmail.com>
2023-12-13 21:11:39 +0500

    Checkout Ui Finished

---
+++ app/src/main/java/com/ikpydev/ecommerceapp/presention/checkout/CheckoutViewModel.kt
@@ -0,0 +1,7 @@
+package com.ikpydev.ecommerceapp.presention.checkout
+
+import com.ikpydev.ecommerceapp.databinding.CardFragmentBinding
+import com.ikpydev.ecommerceapp.utils.BaseFragment
+
+class CheckoutViewModel:BaseFragment<CardFragmentBinding>(CardFragmentBinding::inflate) {
+}
