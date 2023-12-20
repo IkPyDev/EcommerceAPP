1c96d675914193f5647b6ff5c3ae312df1b2920d
IkPyDev <Ikpydev@gmail.com>
2023-12-20 23:11:05 +0500

    Checkout Finish

--- app/src/main/java/com/ikpydev/ecommerceapp/presention/checkout/CheckoutFragment.kt
+++ app/src/main/java/com/ikpydev/ecommerceapp/presention/checkout/CheckoutFragment.kt
@@ -1,25 +1,89 @@
 package com.ikpydev.ecommerceapp.presention.checkout
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
+import android.widget.ArrayAdapter
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.ikpydev.ecommerceapp.data.store.BaseStore
+import androidx.navigation.fragment.navArgs
+import com.ikpydev.ecommerceapp.R
 import com.ikpydev.ecommerceapp.databinding.CheckoutFragmentBinding
-import com.ikpydev.ecommerceapp.presention.card.CardFragmentDirections
+import com.ikpydev.ecommerceapp.domain.module.UserInfo
 import com.ikpydev.ecommerceapp.utils.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class CheckoutFragment: BaseFragment<CheckoutFragmentBinding>(CheckoutFragmentBinding::inflate)  {
+@AndroidEntryPoint
+class CheckoutFragment : BaseFragment<CheckoutFragmentBinding>(CheckoutFragmentBinding::inflate) {
+    private val items = arrayOf(
+        "Toshkent", "Andijon", "Namangan", "Farg'ona", "Sirdaryo",
+        "Jizzax", "Samarqand", "Buxoro", "Navoiy", "Qashqadaryo",
+        "Surxondaryo", "Xorazm", "Karakalpakistan"
+    )
+
+    private lateinit var infoUser: UserInfo
+    private val viewModel by viewModels<CheckoutViewModel>()
+    private val args by navArgs<CheckoutFragmentArgs>()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initUi()
+        subscribeToLiveData()
     }
 
-    private fun initUi()= with(binding) {
+    private fun subscribeToLiveData() = with(binding) {
+        viewModel.userInfo.observe(viewLifecycleOwner) { user: UserInfo ->
+            infoUser = user
+            fullName.text = Editable.Factory.getInstance().newEditable(user.fullName)
+            email.text = Editable.Factory.getInstance().newEditable(user.emailAddress)
+            phone.text = Editable.Factory.getInstance().newEditable(user.phoneNumber)
+            addAddress.text = Editable.Factory.getInstance().newEditable(user.address)
+            zip.text = Editable.Factory.getInstance().newEditable(user.code)
+            city.text = Editable.Factory.getInstance().newEditable(user.city)
+
+            val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
+
+
+
+            count.setAdapter(adapter)
+            count.setText(user.country, false)
+
+
+        }
+    }
+
+    private fun initUi() = with(binding) {
+        back.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
+
+        count.setAdapter(adapter)
+
         next.setOnClickListener {
-            findNavController().navigate(CheckoutFragmentDirections.checkoutPayFragment())
+            if (save.isChecked) {
+
+                val info = UserInfo(
+                    fullName = fullName.text.toString(),
+                    emailAddress = email.text.toString(),
+                    phoneNumber = phone.text.toString(),
+                    address = addAddress.text.toString(),
+                    code = zip.text.toString(),
+                    city = city.text.toString(),
+                    country = count.text.toString(),
+                    promo = args.promo
+
+
+                )
+                infoUser = info
+                viewModel.saveUser(info)
+            }
+
+            findNavController().navigate(CheckoutFragmentDirections.checkoutPayFragment(infoUser))
         }
 
     }
+
+
 }
--- app/src/main/java/com/ikpydev/ecommerceapp/presention/checkout/CheckoutViewModel.kt
+++ app/src/main/java/com/ikpydev/ecommerceapp/presention/checkout/CheckoutViewModel.kt
@@ -1,8 +1,41 @@
 package com.ikpydev.ecommerceapp.presention.checkout
 
-import com.ikpydev.ecommerceapp.databinding.CardFragmentBinding
-import com.ikpydev.ecommerceapp.databinding.CheckoutFragmentBinding
-import com.ikpydev.ecommerceapp.utils.BaseFragment
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ikpydev.ecommerceapp.domain.module.Card
+import com.ikpydev.ecommerceapp.domain.module.UserInfo
+import com.ikpydev.ecommerceapp.domain.repo.OrderRepository
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class CheckoutViewModel{
+
+@HiltViewModel
+class CheckoutViewModel @Inject constructor(
+    private val orderRepository: OrderRepository
+):ViewModel(){
+
+
+
+    val userInfo = MutableLiveData<UserInfo>()
+    init {
+        getUser()
+    }
+
+
+    private fun getUser() = viewModelScope.launch {
+        userInfo.postValue( orderRepository.getUser())
+
+
+        }
+
+
+    fun saveUser(userInfo: UserInfo)=viewModelScope.launch{
+        orderRepository.setUser(userInfo)
+    }
+
 }
